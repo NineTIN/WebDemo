0a7ec738ef9b75692a682dc08405dfb087695359
NineTIN <ninetinxix@gmail.com>
2017-04-25 18:24:37 +0900

    DAO Test & View Test

---
+++ WebDemo/src/main/java/ninetin/sample/dao/SampleDAO.java
@@ -0,0 +1,17 @@
+package ninetin.sample.dao;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Repository;
+
+import ninetin.common.dao.DAO;
+
+@Repository("sampleDAO")
+public class SampleDAO extends DAO{
+
+	public List<Map<String, Object>> selectBoardList(Map<String, Object> map) {
+		// TODO Auto-generated method stub
+		return (List<Map<String, Object>>)selectList("sample.selectBoardList", map);
+	}
+}
