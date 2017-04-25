1670ee8f8dc5c46b657c3e3fb63ca372809371e9
NineTIN <ninetinxix@gmail.com>
2017-04-25 17:49:17 +0900

    JDBC_Test
    MySQL Test File

---
+++ Initialize_Test.sql
@@ -0,0 +1,11 @@
+CREATE TABLE TB_BOARD (
+IDX int unsigned NOT NULL auto_increment,
+PARENT_IDX int,
+TITLE varchar(100) NOT NULL,
+CONTENTS varchar(4000) NOT NULL,
+HIT_CNT int NOT NULL,
+DEL_GB varchar(1) NOT NULL DEFAULT 'N',
+CREA_DTM datetime NOT NULL,
+CREA_ID varchar(30) NOT NULL,
+PRIMARY KEY (IDX)
+);
